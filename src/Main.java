a2a88c33a1ac7d63d343d71a1c1f73d736218390
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2022-12-12 00:57:24 +0300

    ДЗ!!!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-=======
         System.out.println("ДЗ ВЫПОЛНЕНО!");
->>>>>>> 639293e (ДЗ!!!)
     }
 }
